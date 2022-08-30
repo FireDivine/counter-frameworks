c978070496d870b93ad642d9a70affb74c93dedf
Jillian <firedivine46@gmail.com>
2022-08-30 12:42:07 +1200

    added new main parts

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from './Nav'
+
+const Header = () => {
+  return (
+   <header>
+    <Nav/>
+    <h1>Basic Boi</h1>
+   </header>
+  )
+}
+
+export default Header
---
+++ src/components/Nav.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav>
+      <div>
+      <h2>Jellybean</h2>
+      </div>
+     <div>
+      <ul>
+        <li>about</li>
+        <li>another page</li>
+        <li>yet another page</li>
+      </ul>
+     </div>
+    </nav>
+  )
+}
+
+export default Nav
